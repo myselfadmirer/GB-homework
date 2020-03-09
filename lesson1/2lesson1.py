c5b6da23b11d88c53daaa640f19febda7bff0954
myselfadmirer <61723130+myselfadmirer@users.noreply.github.com>
2020-03-09 21:58:22 +0300

    Исправлено по замечаниям

--- lesson1/2lesson1.py
+++ lesson1/2lesson1.py
@@ -2,10 +2,11 @@
 
 time_sec = input('Введите время в секундах ')
 if time_sec.isdigit() and int(time_sec) > 0:
-    ss = int(time_sec) % 60
-    mm = int(time_sec) // 60 % 60
-    hh = int(time_sec) // 60 // 60 % 24
+    time_sec = int(time_sec)
+    ss = time_sec % 60
+    mm = time_sec // 60 % 60
+    hh = time_sec // 60 // 60
 else:
     print('Ничего не выйдет, должно быть введено целое положительное число')
 
-print(f'{hh}:{mm}:{ss}')
+print(f'{hh:02}:{mm:02}:{ss:02}')
