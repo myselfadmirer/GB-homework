0cff603c4ac8c8e19d2bb87b5b4be61ead11e3aa
myselfadmirer <61723130+myselfadmirer@users.noreply.github.com>
2020-03-23 21:26:21 +0300

    Решение задач к 6 уроку

--- lesson6/3lesson6.py
+++ lesson6/3lesson6.py
@@ -7,18 +7,22 @@
 
 
 class Worker:
-    _income = {'wage': input('Введите оклад\n'), 'bonus': input('Введите бонус\n')}
 
-    def __init__(self, name: str, surname: str, position: str):
+    def __init__(self, name: str, surname: str, position: str, wage: float, bonus: float):
+        _income = {
+            'wage': wage,
+            'bonus': bonus,
+        }
         self.name = name
         self.surname = surname
         self.position = position
+        self._income = _income
 
 
 class Position(Worker):
 
-    def __init__(self, name, surname, position):
-        super().__init__(name, surname, position)
+    def __init__(self, name, surname, position, wage, bonus):
+        super().__init__(name, surname, position, wage, bonus)
 
     def get_full_name(self):
         return f'{self.name} {self.surname}, должность - {self.position}'
@@ -28,7 +32,7 @@ class Position(Worker):
         return f'Доход: {total_income} рублей'
 
 
-worker1 = Position('Александр', 'Ветров', 'Инженер')
+worker1 = Position('Александр', 'Ветров', 'Инженер', 40000, 15000)
 print(worker1.get_full_name())
 print(worker1.get_total_income())
 print(worker1._income)
